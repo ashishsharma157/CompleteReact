7b26103c241ebb2490bdb31226262ee71938dbeb
ashishsharma157 <sharma.ashish78@gmail.com>
2024-09-26 12:52:16 -0700

    backend

--- src/component/PostList.jsx
+++ src/component/PostList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import classes from "./PostList.module.css";
@@ -6,16 +6,29 @@ import Modal from "./Modal";
 
 function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
-  // function handlerPost(event)
-  // {
-
-  // }
+  useEffect(() => {
+    async function fetchPost() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+      console.log(resData);
+      setIsFetching(false);
+    }
+    fetchPost();
+  }, []);
 
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((existingPost) => [postData, ...existingPost]);
-    console.log(posts);
-    //setPosts(postData,...posts);
   }
 
   return (
@@ -26,11 +39,11 @@ function PostList({ isPosting, onStopPosting }) {
         </Modal>
       )}
       <ul className={classes.posts}>
-        {posts.map((post, index) => (
-          <Post key={index} author={post.author} body={post.body} />
-        ))}
-        {/* <Post author="Ashish" body="At least i learned basic React" />
-        <Post author="Ashish Sharma" body="React is awesome!!" /> */}
+        {!isFetching &&
+          posts.length > 0 &&
+          posts.map((post, index) => (
+            <Post key={index} author={post.author} body={post.body} />
+          ))}
       </ul>
     </>
   );
